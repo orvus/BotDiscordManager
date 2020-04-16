02ce3636ef7878a90c0bdf67b91986898eed8919
aurelien <aurelien_du_77380@hotmail.fr>
2020-04-17 01:08:58 +0200

    better add and del gestion

--- ConvHelper2.py
+++ ConvHelper2.py
@@ -66,23 +66,59 @@ async def add_role(ctx, id_msg : int , emoji, role, type_):
         await ctx.send("reaction role added")
         save_to_react()
 
-@bot.command(name='role-del')
+
+
+# to_rect[msg_id] = [{"emoji" : emoji_id, "roles": [liste roles]}]
+# to_rect[msg_id] = {emoji_id : [liste roles], id2 : roles}
+
+
+@bot.command(name='role')
+async def add_role(ctx, id_msg : int , emoji, role, type_):
+
+    if id_msg not in to_react.keys():
+        to_react[id_msg] = dict()
+    if emoji.id not in to_react[id_msg].keys():
+        to_react[id_msg][emoji.id] = [role]
+    elif role not in to_react[id_msg][emoji.id]:
+        to_react[id_msg][emoji.id].append(role)
+    else:
+        await ctx.send("role already in message")
+    await ctx.send("reaction role added")
+    save_to_react()
+
+@bot.command(name='del-emoji')
 async def del_role(ctx, id_msg, emoji):
     to_del = -1
     if id_msg not in to_react.keys():
         await ctx.send("no role register for this message")
         return
-    for i in range(to_react[id_msg]):
-        if to_react[id_msg][i]["reation"] == emoji.id:
-            to_del = i
-            break
+    if emoji.id not in to_react[id_msg].keys():
+        await ctx.send("emoji not in message")
+        return
+    del to_react[id_msg][emoji.id]
 
-    if to_del != -1:
-        del to_react[id_msg][to_del]
-        await ctx.send("reaction role delete")
-        save_to_react()
-    else:
-        await ctx.send("this reaction is not register for this message")
+@bot.command(name='del-role')
+async def del_role(ctx, id_msg, role):
+    to_del = -1
+    if id_msg not in to_react.keys():
+        await ctx.send("no role register for this message")
+        return
+    for emoji_id,roles in to_react[id_msg].items():
+        if role in roles:
+            to_react[id_msg][emoji_id].remove(role)
+    await ctx.send("emoji not in message")
+
+@bot.command(name='del-emoji-role')
+async def del_emoji_role(ctx, id_msg, emoji, role):
+    if id_msg not in to_react.keys():
+        await ctx.send("no role register for this message")
+        return
+    if emoji.id not in to_react[id_msg].keys():
+        await ctx.send("emoji not in message")
+        return
+    if role in to_react[id_msg][emoji.id]:
+        to_react[id_msg][emoji.id].remove(role)
+    await ctx.send("emoji not in message")
 
 
 @bot.command(name='open-conv')
@@ -112,6 +148,8 @@ async def start_bot(ctx):
 
     #await ctx.send(discord.utils.escape_mentions(s)[:1999])
 
+def get_element(emo_id):
+
 
 @bot.event
 async def on_raw_reaction_add(reactionPayload):
@@ -122,20 +160,13 @@ async def on_raw_reaction_add(reactionPayload):
     guild = member.guild
     print(emoji)
     if msg_id in to_react.keys():
+        rr = None
+        for r in to_react[msg_id]:
+            if emoji.id == r["reaction"]:
+                rr = r
 
-        try:
-            test = to_react[msg_id]["reaction"].split(":")[1]
-            print("1", emoji, emoji.name, test, emoji.name == test)
-            role = discord.utils.get(guild.roles, name=to_react[msg_id]["role"])
-            await member.add_roles(role)
-            return
-        except:
-            print("ok")
-
-        if to_react[msg_id]["reaction"] == emoji.name:
-            print("2")
+        if rr is not None:
             if to_react[msg_id]["type"] in [1, 2]:
-                print("3")
                 print(f"give the role {to_react[msg_id]['role']} to {member.display_name}")
                 role = discord.utils.get(guild.roles, name=to_react[msg_id]["role"])
                 print(f"ROLE : {to_react[msg_id]['role']}")
