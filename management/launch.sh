f3c351fe77099385f40e805e03cba9ff47824303
aurelien <aurelien_du_77380@hotmail.fr>
2020-11-07 14:11:30 +0100

    start work on launch script

---
+++ management/launch.sh
@@ -0,0 +1,48 @@
+#/bin/bash
+
+log_dir = "../log/"
+log_channel = "chanManager.log"
+log_reaction = "reactManager.log"
+log_siteswap = "siteswap.log"
+
+script_dir = "../"
+
+
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --chan)
+      chan=1
+      ;;
+    --react)
+      react=1
+      ;;
+    --siteswap)
+      siteswap=1
+      ;;
+    --help|-h)
+      printf "Meaningful help message" # Flag argument
+      exit 0
+      ;;
+    *)
+      >&2 printf "Error: Invalid argument\n"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+touch ${log_dir}${log_reaction}
+touch ${log_dir}${log_siteswap}
+touch ${log_dir}${log_channel}
+
+
+echo > ${log_dir}${log_reaction}
+echo > ${log_dir}${log_siteswap}
+
+if [[ ${chan}==1 ]]
+then
+    echo > "${log_dir}${log_channel}"
+    nohup "${script_dir}channelManager.py" > "${log_dir}${log_channel}"
+fi
+
