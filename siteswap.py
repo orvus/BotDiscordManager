e74a037e3201bc72efc22871706824a0de432925
aurelien <aurelien_du_77380@hotmail.fr>
2021-06-28 22:40:30 +0200

    ajout de Path pour chemin linux et windows

--- siteswap.py
+++ siteswap.py
@@ -1,16 +1,11 @@
 # bot.py
 import os
-import subprocess
-import asyncio
-import random
-import time
+from pathlib import Path
 import discord
 from discord.ext import commands
-import copy
 from dotenv import load_dotenv
-import urllib.request
 import hashlib
-import wget
+
 
 load_dotenv(dotenv_path=".env2")
 
@@ -27,32 +22,43 @@ some example :\n
 +siteswap (4,2x)(2x,4)
 +siteswap [333]33
 +siteswap <3p|3p><3|3><3|3><3|3>""")
-async def siteswap(ctx, *,siteswap="333"):
+async def siteswap(ctx, *, siteswap = "333"):
 
-    if ctx.channel.id not in [702470629198266388]:
-        await ctx.message.delete(delay=1.0)
-        print("pas le droit d'ecrire")
-        return
+    ## uncomment and put a chan id if you xant to limit the usage of the bot to one discord channel
+    #if ctx.channel.id not in [702470629198266388]:
+        #await ctx.message.delete(delay=1.0)
+        #print("pas le droit d'ecrire")
+        #return
 
     siteswap = siteswap.replace(" ", "")
     siteswapEnc = siteswap.encode()
-    #ret = os.system(f"./JugglingLab/jlab.bat togif \"{siteswap}\" -out plop.gif")
-    print(f" hash({siteswap}) = {hashlib.sha256(siteswapEnc).hexdigest() } ")
-    if os.path.exists(f"./Siteswap/{hashlib.sha256(siteswapEnc).hexdigest()}.gif") is False:
-        #result = subprocess.run([".\\JugglingLab\\jlab.bat", "togif", f"{siteswap}", "-out", f".\\Siteswap\\{hashlib.sha256(siteswapEnc).hexdigest()}.gif"])
-        print(f"./JugglingLab/jlab togif \"pattern={siteswap}\" -prefs \"slowdown=2.9;fps=33.3\" -out ./Siteswap/{hashlib.sha256(siteswapEnc).hexdigest()}.gif")
-        os.system(f"./JugglingLab/jlab togif \"pattern={siteswap}\" -prefs \"slowdown=2.9;fps=33.3\" -out ./Siteswap/{hashlib.sha256(siteswapEnc).hexdigest()}.gif")
-        #print(f"{result.args}") -prefs \"width=280;height=320;slowdown=1.0;fps=33.3\"
-        #print(f"{result.stderr} | {result.stdout} ")
 
+    #the hash of the siteswap is use for known if it has already been search, generated and store
+    print(f" hash({siteswap}) = {hashlib.sha256(siteswapEnc).hexdigest() } ")
+
+    imagePath = Path(f"./Siteswap/{hashlib.sha256(siteswapEnc).hexdigest()}.gif")
+
+    # if the siteswap has not been already use
+    # use juggling lab for create GIF and store it for speed the next load
+    if imagePath.exists() is False:
+        cmd = Path("JugglingLab/jlab")
+        arg1 = "togif"
+        arg2 = f"\"pattern={siteswap}\""
+        arg3 = "-prefs \"slowdown=2.9;fps=33.3\""
+        arg4 = "-out " + str(imagePath)
+
+        print(f"{cmd} {arg1} {arg2} {arg3} {arg4} ")
+        os.system(f"{cmd} {arg1} {arg2} {arg3} {arg4}")
+    #if the GIF is in local
     else:
         print("siteswap already exist")
-    if(os.path.exists(f"./Siteswap/{hashlib.sha256(siteswapEnc).hexdigest()}.gif")):
+
+    # at this moment, the image should exists, if not, it's a failure
+    if imagePath.exists():
         print("file exist")
         await ctx.send(content=f"{ctx.author.mention} voila le siteswap {siteswap} ", file=discord.File(f"./Siteswap/{hashlib.sha256(siteswapEnc).hexdigest()}.gif"))
     else:
         print("fail")
-
         await ctx.send(content=f"voila le siteswap {siteswap}",file=discord.File("./Siteswap/juggler.gif"), delete_after=10.0)
         await ctx.send(content=f"**OU PAS**, voici le lien de juggling lab pour apprendre le siteswap : https://jugglinglab.org/html/ssnotation.html ", delete_after=10.0)
     try:
@@ -60,10 +66,5 @@ async def siteswap(ctx, *,siteswap="333"):
     except:
         print("no msg to del")
     await ctx.send(content=f"pour m'utiliser fait +siteswap <ton siteswap>\nexemple : +siteswap 441")
-    #os.remove(f".\\Siteswap\\{hashlib.sha256(siteswapEnc).hexdigest()}.gif")
 
 bot.run(TOKEN)
-
-
-
-#"@gym siteswap généré alea ou list de siteswap compliqué"
